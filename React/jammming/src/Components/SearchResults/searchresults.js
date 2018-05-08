fa98e532f16510ce8c559a0fa93130672b5238c4
Owowowmyface <alyea.greg@gmail.com>
2018-05-07 21:46:07 -0400

    Jammming: Fixed some incorrectly placed comments that caused stuff to break. Oops

--- React/jammming/src/Components/SearchResults/searchresults.js
+++ React/jammming/src/Components/SearchResults/searchresults.js
@@ -3,11 +3,11 @@ import './searchresults.css';
 import {TrackList} from '../TrackList/tracklist';
 
 export class SearchResults extends React.Component {
+  //searchlist is relatively unremarkable. It renders a tracklist and passes down a few methods/parameters
   render() {
     return (
       <div className="SearchResults">
         <h2>Results</h2>
-        //searchlist is relatively unremarkable. It renders a tracklist and passes down a few methods/parameters
           <TrackList tracks={this.props.searchResults} onAdd={this.props.onAdd} isRemoval={false}/>
       </div>
     )};
--- React/jammming/src/Components/TrackList/tracklist.js
+++ React/jammming/src/Components/TrackList/tracklist.js
@@ -3,10 +3,10 @@ import './tracklist.css';
 import {Track} from '../Track/track';
 
 export class TrackList extends React.Component {
+      //displays a list of tracks. Gets rendered by SearchResults and PlayList
   render() {
     return (
       <div className="TrackList">
-      //displays a list of tracks. Gets rendered by SearchResults and PlayList
         {this.props.tracks.map(track => <Track track={track} key={track.id} onAdd={this.props.onAdd} onRemove={this.props.onRemove} isRemoval={this.props.isRemoval}/>)}
       </div>
     );
